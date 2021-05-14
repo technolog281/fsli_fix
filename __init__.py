c4f5901868df0e279e64d62b4e2de857539bb9cf
galkovskiy <dr.galkovsky2017@gmail.com>
2021-05-14 17:14:09 +0500

    Выпилен метод из наименования параметра

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from typing import Dict, Any
-
 from fastapi import FastAPI
 from uvicorn import run
 from jinja2 import Template
@@ -11,9 +10,8 @@ from openpyxl import load_workbook
 from search_last_list import last_page
 import pandas as pd
 
-wb = load_workbook('./names_v6.xlsx')
+wb = load_workbook('./names_v7.xlsx')
 sheet = wb['Sheet1']
-
 
 
 def values(num):
@@ -29,68 +27,19 @@ def values(num):
     return result
 
 
-belbol = pyodbc.connect("DRIVER={SQL Server};"
-                        "SERVER=DROID1347\SQL_MIS;"
-                        "DATABASE=amur_mis_belbol;"
-                        "UID=sa;"
-                        "PWD=Apacheserver1390")
-belbol_cursor = belbol.cursor()
-# amur_mis_belbol = amur_mis_belbol_conn.setencoding(encoding='utf-8')
+# belbol = pyodbc.connect("DRIVER={SQL Server};"
+#                         "SERVER=DROID1347\SQL_MIS;"
+#                         "DATABASE=amur_mis_belbol;"
+#                         "UID=sa;"
+#                         "PWD=Apacheserver1390")
+# belbol_cursor = belbol.cursor()
+# amur_mis_belbol = amur_mis_belbol_conn.setencoding(encoding='utf-8') ## -- Декодирование
 
 
-script_base = pyodbc.connect('DRIVER={SQL Server};'
-                             'SERVER=DROID1347\SQL_MIS;'
-                             'DATABASE=script_base;'
-                             'UID=sa;'
-                             'PWD=Apacheserver1390')
-sb_cursor = script_base.cursor()
-# script_base = amur_mis_belbol_conn.setencoding(encoding='utf-8')
-
-
-# app = FastAPI()
-# eng = create_engine('DRIVER={ODBC Driver 17 for SQL Server};'
-#                     'SERVER=DROID1347\SQL_MIS;'
-#                     'DATABASE=amur_mis_belbol;'
-#                     'UID=sa;'
-#                     'PWD=Apacheserver1390')
-# import urllib
-# eng = 'DRIVER={SQL Server};DATABASE=amur_mis_belbol;SERVER=DROID1347\SQL_MIS;UID=sa;PWD=Apacheserver1390'
-# eng = urllib.quote_plus(eng)
-# eng = "mssql+pyodbc:///?odbc_connect=%s" % eng
-#
-# eng = create_engine('mssql+pyodbc://sa:Apacheserver1390@127.0.0.1:2044/SQL_MIS/amur_mis_belbol?driver=SQL+Server', pool_pre_ping=True)
-# jdbc:sqlserver://127.0.0.1\SQL_MIS:2044;database=amur_mis_belbol
-# with eng.connect() as con:
-#     rs = con.execute('select * from lbr_Laboratory')
-#
-#     data = rs.fetchone()[0]
-#
-#     print
-#     "Data: %s" % data
-
-# qwe = "select * from lbr_ResearchTypeParam"
-#
-#
-# def read_root(ask):
-#     cursor.execute(ask)
-#     row = cursor.fetchall()
-#     # nums = list(range(1, len(row) + 1))
-#     # new_dict = {nums: row for nums, row in zip(nums, row)}
-#     return row
-#
-#
-# print(type(read_root(qwe)))
-#
-# def answ():
-#     html = open("C:\\Users\\Droid1347\\PycharmProjects\\pythonProject\\html\\_form.html").read()
-#     template = Template(html)
-#     return template.render(content=html, status_code=200)
-#
-#
-# @app.get("/items/", response_class=HTMLResponse)
-# async def read_items():
-#     return answ()
-#
-#
-# if __name__ == '__main__':
-#     run(app, host='localhost', port=8080)
+# script_base = pyodbc.connect('DRIVER={SQL Server};'
+#                              'SERVER=DROID1347\SQL_MIS;'
+#                              'DATABASE=script_base;'
+#                              'UID=sa;'
+#                              'PWD=Apacheserver1390')
+# sb_cursor = script_base.cursor()
+# script_base = amur_mis_belbol_conn.setencoding(encoding='utf-8') ## -- Декодирование
---
+++ type_select.py
@@ -0,0 +1,38 @@
+from __init__ import *
+
+wb = load_workbook('./names_v7.xlsx')
+sheet = wb['Sheet1']
+df = pd.DataFrame(
+    columns=['NUM', 'RESEARCH_TYPE', 'RESEARCH_PARAM', 'NMU', 'BIOM', 'UNIT', 'LIS_CODE', 'MAT_CODE', 'LAB_TYPE'])
+
+
+def count(rp_split):
+    for i in rp_split:
+        if 'метод' in i:
+            return rp_split.index(i)
+
+
+for num in range(2, 23458):
+    val = values(num)
+    rp_split = val['RESEARCH_PARAM'].split(' ')
+    pos = count(rp_split)
+    len_split = len(rp_split)
+    if pos is not None and pos + 1 == len_split:
+        rp = rp_split[pos - 1] + ' ' + rp_split[pos]
+        val['RESEARCH_PARAM'] = rp
+        df = df.append(val, ignore_index=True, sort=False)
+    elif pos is not None and pos + 1 != len_split:
+        rp = ''
+        for i in range(pos, len_split):
+            rp = rp + ' ' + rp_split[i]
+            val['RESEARCH_PARAM'] = rp
+        df = df.append(val, ignore_index=True, sort=False)
+    else:
+        df = df.append(val, ignore_index=True, sort=False)
+
+
+df.to_excel('probe.xlsx')
+
+
+
+
