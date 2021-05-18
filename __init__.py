6889fb2d52a845c26c3eff45968a7c1d2224fbb9
Droid1347 <dr.galkovsky2017@gmail.com>
2021-05-18 07:56:55 +0500

    rev 0.1

--- __init__.py
+++ __init__.py
@@ -1,38 +1,38 @@
-import pyodbc
-from openpyxl import load_workbook
-import loguru as log
-from search_last_list import last_page
-import pandas as pd
-
-wb = load_workbook('./probe.xlsx')
-sheet = wb['Sheet1']
-
-
-def values(num):
-    result = {'NUM': sheet[f'A{num}'].value,
-              'RESEARCH_TYPE': sheet[f'B{num}'].value,
-              'RESEARCH_PARAM': sheet[f'C{num}'].value,
-              'RTKID': sheet[f'D{num}'].value,
-              'BIOM': sheet[f'E{num}'].value,
-              'UNIT': sheet[f'F{num}'].value,
-              'LIS_CODE': sheet[f'G{num}'].value,
-              'LAB_TYPE': sheet[f'H{num}'].value}
-    return result
-
-
-belbol = pyodbc.connect("DRIVER={SQL Server};"
-                        "SERVER=DROID1347\SQL_MIS;"
-                        "DATABASE=amur_mis_belbol;"
-                        "UID=sa;"
-                        "PWD=Apacheserver1390")
-belbol_cursor = belbol.cursor()
-# amur_mis_belbol = amur_mis_belbol_conn.setencoding(encoding='utf-8') ## -- Декодирование
-
-
-script_base = pyodbc.connect('DRIVER={SQL Server};'
-                             'SERVER=DROID1347\SQL_MIS;'
-                             'DATABASE=script_base;'
-                             'UID=sa;'
-                             'PWD=Apacheserver1390')
-sb_cursor = script_base.cursor()
-# script_base = amur_mis_belbol_conn.setencoding(encoding='utf-8') ## -- Декодирование
+# import pyodbc
+# from openpyxl import load_workbook
+# import loguru as log
+# from search_last_list import last_page
+# import pandas as pd
+#
+# wb = load_workbook('./probe.xlsx')
+# sheet = wb['Sheet1']
+#
+#
+# def values(num):
+#     result = {'NUM': sheet[f'A{num}'].value,
+#               'RESEARCH_TYPE': sheet[f'B{num}'].value,
+#               'RESEARCH_PARAM': sheet[f'C{num}'].value,
+#               'RTKID': sheet[f'D{num}'].value,
+#               'BIOM': sheet[f'E{num}'].value,
+#               'UNIT': sheet[f'F{num}'].value,
+#               'LIS_CODE': sheet[f'G{num}'].value,
+#               'LAB_TYPE': sheet[f'H{num}'].value}
+#     return result
+#
+#
+# belbol = pyodbc.connect("DRIVER={SQL Server};"
+#                         "SERVER=DROID1347\SQL_MIS;"
+#                         "DATABASE=amur_mis_belbol;"
+#                         "UID=sa;"
+#                         "PWD=Apacheserver1390")
+# belbol_cursor = belbol.cursor()
+# # amur_mis_belbol = amur_mis_belbol_conn.setencoding(encoding='utf-8') ## -- Декодирование
+#
+#
+# script_base = pyodbc.connect('DRIVER={SQL Server};'
+#                              'SERVER=DROID1347\SQL_MIS;'
+#                              'DATABASE=script_base;'
+#                              'UID=sa;'
+#                              'PWD=Apacheserver1390')
+# sb_cursor = script_base.cursor()
+# # script_base = amur_mis_belbol_conn.setencoding(encoding='utf-8') ## -- Декодирование
--- select_rtk.py
+++ select_rtk.py
@@ -1,22 +1,79 @@
-from __init__ import *
+import pandas as pd
+# from __init__ import *
+# from encodings import ascii as us-ascii
 from openpyxl import load_workbook
+from openpyxl import Workbook
 
-wb = load_workbook('./probe.xlsx')
-sheet = wb['Sheet1']
-
-for num in range(2, 4):
-    worker1 = values(num)['RESEARCH_PARAM']
-    worker2 = values(num)['RESEARCH_TYPE']
-    worker3 = values(num)['RTKID']
-    if worker1 == 'подтверждающим методом':
-        worker2 = worker2 + ' ' + 'подтверждающим методом'
-        worker3 = '10'
-        worker1 = ''
-    elif worker1 == 'предвариательным методом':
-        worker2 = worker2 + ' ' + 'предвариательным методом'
-        worker3 = '10'
-        worker1 = ''
-    elif worker1 == 'предвариательным методом':
-        worker2 = worker2 + ' ' + 'предвариательным методом'
-        worker3 = '10'
-        worker1 = ''
+wb = load_workbook('./names_v5.xlsx')
+sheet_1 = wb['Sheet1']
+num = 1
+values_2 = {'G_NAME': str(sheet_1[f'A{num}'].value),
+            'NAME': str(sheet_1[f'B{num}'].value),
+            'CODE': str(sheet_1[f'C{num}'].value)}
+# values_2 = [str(sheet_1[f'A{num}'].value), str(sheet_1[f'B{num}'].value), str(sheet_1[f'C{num}'].value)]
+num_symbols = ('1', '2', '3', '4', '5', '6', '7', '(')
+wb_ext = Workbook('names_upgrade.xlsx')
+ws = wb_ext.create_sheet('Sheet1')
+print(values_2['G_NAME'])
+ws.append([values_2['G_NAME'], values_2['NAME'], values_2['CODE']])
+#
+#
+# for num in range(2, 200):
+#     start_g_name = values_2['G_NAME']
+#     start_name = values_2['NAME']
+#     if 'производн' in start_g_name:
+#         values_2['CODE'] = '200'
+#         ws.append([values_2['G_NAME'], values_2['NAME'], values_2['CODE']])
+#         # ws.append(values_2['NAME'])
+#         # ws.append(values_2['CODE'])
+#         # ws[f'A{num}'] = values_2['G_NAME']
+#         # ws[f'B{num}'] = values_2['NAME']
+#         # ws[f'C{num}'] = values_2['CODE']
+#     elif 'Днк' or 'Рнк' in start_name:
+#         values_2['CODE'] = '208'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'иммунофермент' in start_name:
+#         values_2['CODE'] = '206'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'иммунохромат' in start_name:
+#         values_2['CODE'] = '204'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'рН' in start_g_name:
+#         values_2['CODE'] = '200'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'Массовая концентрация' or 'Массовое содержание' in start_name:
+#         values_2['CODE'] = '200'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'ромбоцит' or 'ейкоцит' or 'ритроцит' or 'имфорцит' in start_g_name:
+#         values_2['CODE'] = '202'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'бнаружени' in start_name:
+#         values_2['CODE'] = '208'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'иммунофлуор' in start_name:
+#         values_2['CODE'] = '207'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'иммунохеми' in start_name:
+#         values_2['CODE'] = '207'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'иммунолог' in start_name:
+#         values_2['CODE'] = '204'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'ремя' in start_name:
+#         values_2['CODE'] = '201'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'микроорган' or 'ультур' in start_name:
+#         values_2['CODE'] = '208'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'язкост' or 'Запах' or 'Цвет' in start_name:
+#         values_2['CODE'] = '201'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     elif 'олярная' in start_name:
+#         values_2['CODE'] = '200'
+#         # rf.append(values_2, ignore_index=True, sort=False)
+#     # else:
+#     #     # rf.append(values_2, ignore_index=True, sort=False)
+#
+# # print(rf)
+# # df.to_excel('names_v9.xlsx')
+#
